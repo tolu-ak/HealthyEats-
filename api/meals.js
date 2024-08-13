a72d4c9b075f3712cfb212e584a113906ac900ea
iyanuoluwa sowande <iyanusowande@gmail.com>
2024-08-13 12:15:37 -0700

    fix sytax error

--- api/meals.js
+++ api/meals.js
@@ -1,28 +1,25 @@
+const express = require('express');
+const crypto = require('crypto');
+// const { connection } = require('./conn');
 
-const express = require('express')
-const crypto = require('crypto')
-const { connection } = require('./conn')
+const mealRouter = express.Router();
 
-const mealRouter = express.Router()
+mealRouter.post('/mealsearch', (req, res) => {
+  const meal = req.body.meal;
+  console.log(meal);
 
-mealRouter.post('/mealsearch', (req,res) => {
-    const meal = req.body.meal;
-    console.log(meal);
-    
-
-    //select meal and its components from database
-    const sql = `
+  //select meal and its components from database
+  const sql = `
     SELECT * FROM nigerianfoods WHERE name = ?
     UNION ALL
     SELECT * FROM alternativeoptions WHERE name = ?
 `;
 
-connection.query(sql, [req.body.meal, req.body.meal], function (err, result) {
-    if (err) throw err;
-    console.log(result);
-    res.json(result);
-}); 
+  //   connection.query(sql, [req.body.meal, req.body.meal], function (err, result) {
+  //     if (err) throw err;
+  //     console.log(result);
+  //     res.json(result);
+  //   });
+});
 
 module.exports = mealRouter;
-
-});
