01073f9254998b211fbc5fb8657163ca7821203c
T'OLU AKINWANDE <toluwaniakinwande@gmail.com>
2024-08-15 15:47:21 -0700

    Added database files

--- api/conn.js
+++ api/conn.js
@@ -1,28 +1,23 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Titilayo123!",
-    database: "healthyeatswebsite",
-    port: "3306" //default mysql port
-     
-    });
-    
-    // connect to database
+  host: "localhost",
+  user: "root",
+  password: "Titilayo123!",
+  database: "healthyeatswebsite",
+  port: "3306", //default mysql port
+});
 
-    connection.connect((err) => {
-        if (err) {
-            console.error('Error connecting to the database:', err.stack);
-            return;
-        }
-        console.log('Connected to the database as id ' + connection.threadId);
-    });
+// connect to database
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.stack);
+    return;
+  }
+  console.log("Connected to the database as id " + connection.threadId);
+});
 
 module.exports = {
-    connection
-}
-
-
-
-  
+  connection,
+};
