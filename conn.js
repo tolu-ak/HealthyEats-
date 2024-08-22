8b52bb43abb8fac87315a42d481e86109de1e965
T'OLU AKINWANDE <toluwaniakinwande@gmail.com>
2024-08-22 11:10:01 -0700

    Update

--- conn.js
+++ conn.js
@@ -3,15 +3,11 @@ const mysql = require('mysql2');
 console.log(process.env.DBHOSTNAME, process.env.DBUSER, process.env.PASSWORD);
 
 const connection = mysql.createConnection({
-  // host: process.env.DBHOSTNAME,
-  // user: process.env.DBUSER,
-  // password: process.env.PASSWORD,
-  // database: process.env.DBNAME,
- host: "localhost",
-    user: "root",
-    password: "Titilayo123!",
-    database: "healthyeatswebsite",
-    port: "3306"
+  host: process.env.DBHOSTNAME,
+  user: process.env.DBUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DBNAME,
+  port: process.env.DBPORT,
 });
 
 // connect to database
